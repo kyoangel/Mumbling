31345374bb36ee2a055a020c4f1a73bbd2cccf7c
kyoangel <gd92x2@hotmail.com>
2017-07-24 21:41:05 +0800

    [refactor] simple refactor

--- RefactorMumbling/Mumbling.cs
+++ RefactorMumbling/Mumbling.cs
@@ -15,28 +15,28 @@ namespace RefactorMumbling
         public string Accum(string s)
         {
             // your code
-            if (s.Length == 0)
-                return "";
-            if (s.Length == 1)
-                return s.ToUpper();
-            if (s.Length == 2)
-                return s[0].ToString().ToUpper() + "-" + s[1].ToString().ToUpper() + s[1].ToString().ToLower();
-
             var result="";
             var upper = "";
             for (int i = 0; i < s.Length; i++)
             {
                 upper = s[i].ToString().ToUpper();
                 var lower = "";
-                for (int j = 0; j < i; j++)
-                {
-                    lower += s[i].ToString().ToLower();
-                }
+                lower = GetLowerChars(s, i, lower);
                 result += upper + lower + "-";
 
             }
 
             return result.TrimEnd('-');
+        }
+
+        private static string GetLowerChars(string currentChar, int times, string lower)
+        {
+            for (int j = 0; j < times; j++)
+            {
+                lower += currentChar[times].ToString().ToLower();
+            }
+
+            return lower;
         }
     }
 }
