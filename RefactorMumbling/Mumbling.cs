5d4c437675d9faa3769af213dc064c49ad0b76e1
kyoangel <gd92x2@hotmail.com>
2017-07-24 21:34:57 +0800

    [Vs2015] create projects and complete function with TDD

---
+++ RefactorMumbling/Mumbling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorMumbling
+{
+    public class Mumbling
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public string Accum(string s)
+        {
+            // your code
+            if (s.Length == 0)
+                return "";
+            if (s.Length == 1)
+                return s.ToUpper();
+            if (s.Length == 2)
+                return s[0].ToString().ToUpper() + "-" + s[1].ToString().ToUpper() + s[1].ToString().ToLower();
+
+            var result="";
+            var upper = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                upper = s[i].ToString().ToUpper();
+                var lower = "";
+                for (int j = 0; j < i; j++)
+                {
+                    lower += s[i].ToString().ToLower();
+                }
+                result += upper + lower + "-";
+
+            }
+
+            return result.TrimEnd('-');
+        }
+    }
+}
---
+++ RefactorMumblingTests/MumblingTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefactorMumbling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorMumbling.Tests
+{
+    [TestClass()]
+    public class MumblingTests
+    {
+        [TestMethod()]
+        public void InputEmptyString_Should_ReturnEmptyString()
+        {
+            CheckExpectString("", "");
+        }
+        [TestMethod]
+        public void InputOneChar_Should_ReturnOneUpperString()
+        {
+            CheckExpectString("a", "A");
+        }
+
+        [TestMethod]
+        public void InputAB_should_Return_A_dash_Bb()
+        {
+            CheckExpectString("AB", "A-Bb");
+        }
+
+        [TestMethod]
+        public void InputABC_Should_Return_A_dash_Bb_Dash_Ccc()
+        {
+            CheckExpectString("ABC", "A-Bb-Ccc");
+        }
+        [TestMethod]
+        public void CodeWarsTests()
+        {
+            CheckExpectString("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+            CheckExpectString("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+            CheckExpectString("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+            CheckExpectString("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+            CheckExpectString("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+        }
+
+        private static void CheckExpectString(string actual, string expect)
+        {
+            Mumbling mubling = new Mumbling();
+            Assert.AreEqual(expect, mubling.Accum(actual));
+        }
+
+    }
+}
